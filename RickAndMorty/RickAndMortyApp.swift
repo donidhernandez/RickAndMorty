2f81930ce0060dab56b0544e95e4d1d89830c7c4
Adonai Domingez <donihernandez003@gmail.com>
2023-02-26 22:32:00 -0500

    Initial Commit

---
+++ RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Doni on 2/26/23.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
